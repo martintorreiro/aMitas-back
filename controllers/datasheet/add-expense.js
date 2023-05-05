3d0fd69ce3e00c847a0102229ad22afe970cd896
Martin <martin.tc.wdev@gmail.com>
2023-05-05 15:12:42 +0200

    kk

--- controllers/datasheet/add-expense.js
+++ controllers/datasheet/add-expense.js
@@ -2,15 +2,16 @@ const addExpenseDb = require("../../db/datasheet/add-expense");
 
 const addExpense = async (req, res, next) => {
   const { userId, concept, amount } = req.body;
-  console.log("--->", userId, concept, amount);
+  console.log("--->",userId,concept,amount)
   try {
     await addExpenseDb(userId, concept, amount);
 
     res.send({
       status: "ok",
-      message: "Usuario añadido a la lista",
+      message: "Gasto añadido",
     });
   } catch (error) {
+    error.message = "Error al registrar el gasto"
     next(error);
   }
 };
